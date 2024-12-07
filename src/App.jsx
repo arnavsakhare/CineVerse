73595498ba5107831054191a07ddc49205860c4e
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-12-07 20:51:15 +0530

    added genres call

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { fetchDataFromApi } from './utils/api'
 import { useDispatch, useSelector } from 'react-redux'
-import { getAPIConfiguration } from './store/homeSlice'
+import { getAPIConfiguration, getGenres } from './store/homeSlice'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './pages/home/Home'
@@ -17,13 +17,37 @@ function App() {
   const { url } = useSelector((state) => state.home)
 
   useEffect(() => {
-    apiTesting()
+    fetchAPIConfig()
+    genresCall()
   }, [])
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular').then((res) => {
-      dispatch(getAPIConfiguration(res))
+  const fetchAPIConfig = () => {
+    fetchDataFromApi('/configuration').then((res) => {
+
+      const url = {
+        backdrop: res.images.secure_base_url + 'original',
+        poster: res.images.secure_base_url + 'original',
+        profile: res.images.secure_base_url + 'original',
+      }
+
+      dispatch(getAPIConfiguration(url))
     })
+  }
+
+  const genresCall = async () => {
+    let promises = []
+    let endPoints = ["tv", "movie"]
+    let allGenres = {}
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`))
+    })
+
+    const data = await Promise.all(promises)
+    data?.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item))
+    })
+    dispatch(getGenres(allGenres))
   }
 
   return (
